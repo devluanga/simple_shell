71abaa4dec9774634a2dd7ecc9b162372ad415a0
kodkurei <kureiallan47@gmail.com>
2023-04-26 16:17:46 +0300

    updated file

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -131,4 +131,4 @@ void help_unsetenv(void);
 void help_history(void);
 
 int proc_file_commands(char *file_path, int *exe_ret);
-#endif /* _SHELL_H_ */
+#endif /* _MAIN_H_ */

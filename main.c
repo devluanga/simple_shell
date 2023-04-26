ebce4f86db59c3914c7692471f39846dc16f001c
devluanga <imaniimaniluanga@gmail.com>
2023-04-26 20:50:27 +0300

    updated  main

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "main.h"
+alias_t *aliases;
+void _prompt(int sig);
+int execute(char **args, char **front);
 
 /**
  * _prompt - Prints a new prompt upon a signal.
@@ -6,11 +9,10 @@
  */
 void _prompt(int sig)
 {
-	char *shell = "\n$ ";
-
-	(void)sig;
-	signal(SIGINT, _prompt);
-	write(STDIN_FILENO, shell, 3);
+char *_shell = "\n$ ";
+(void)sig;
+signal(SIGINT, _prompt);
+write(STDIN_FILENO, _shell, 3);
 }
 
 /**
@@ -23,51 +25,50 @@ void _prompt(int sig)
  */
 int execute(char **args, char **front)
 {
-	pid_t child_pid = fork();
-	int status, flag = 0, ret = 0;
-	char *command = args[0];
-
-	if (command[0] != '/' && command[0] != '.')
-	{
-		flag = 1;
-		command = get_location(command);
-	}
-
-	if (!command || (access(command, F_OK) == -1))
-	{
-		if (errno == EACCES)
-			ret = (create_error(args, 126));
-		else
-			ret = (create_error(args, 127));
-	}
-	else
-	{
-		if (child_pid == -1)
-		{
-			if (flag)
-				free(command);
-			perror("Error child:");
-			return (1);
-		}
-		if (child_pid == 0)
-		{
-			execve(command, args, environ);
-			if (errno == EACCES)
-				ret = (create_error(args, 126));
-			free_env();
-			free_args(args, front);
-			free_alias_list(aliases);
-			_exit(ret);
-		}
-		else
-		{
-			wait(&status);
-			ret = WEXITSTATUS(status);
-		}
-	}
-	if (flag)
-		free(command);
-	return (ret);
+pid_t child_pid;
+int status, flag = 0, ret = 0;
+char *command = args[0];
+if (command[0] != '/' && command[0] != '.')
+{
+flag = 1;
+command = get_location(command);
+}
+if (!command || (access(command, F_OK) == -1))
+{
+if (errno == EACCES)
+ret = (create_error(args, 126));
+else
+ret = (create_error(args, 127));
+}
+else
+{
+child_pid = fork();
+if (child_pid == -1)
+{
+if (flag)
+free(command);
+perror("Error child:");
+return (1);
+}
+if (child_pid == 0)
+{
+execve(command, args, environ);
+if (errno == EACCES)
+ret = (create_error(args, 126));
+free_env();
+free_args(args, front);
+free_alias_list(aliases);
+_exit(ret);
+}
+else
+{
+wait(&status);
+ret = WEXITSTATUS(status);
+}
+}
+if (flag)
+free(command);
+return (ret);
 }
 
 /**
@@ -79,52 +80,46 @@ int execute(char **args, char **front)
  */
 int main(int argc, char *argv[])
 {
-	int ret = 0, retn;
-	int *exe_ret = &retn;
-	char *prompt = "$ ", *new_line = "\n";
-
-	name = argv[0];
-	hist = 1;
-	aliases = NULL;
-	signal(SIGINT, sig_handler);
-
-	*exe_ret = 0;
-	environ = _copyenv();
-	if (!environ)
-		exit(-100);
-
-	if (argc != 1)
-	{
-		ret = proc_file_commands(argv[1], exe_ret);
-		free_env();
-		free_alias_list(aliases);
-		return (*exe_ret);
-	}
-
-	if (!isatty(STDIN_FILENO))
-	{
-		while (ret != END_OF_FILE && ret != EXIT)
-			ret = handle_args(exe_ret);
-		free_env();
-		free_alias_list(aliases);
-		return (*exe_ret);
-	}
-
-	while (1)
-	{
-		write(STDOUT_FILENO, prompt, 2);
-		ret = handle_args(exe_ret);
-		if (ret == END_OF_FILE || ret == EXIT)
-		{
-			if (ret == END_OF_FILE)
-				write(STDOUT_FILENO, new_line, 1);
-			free_env();
-			free_alias_list(aliases);
-			exit(*exe_ret);
-		}
-	}
-
-	free_env();
-	free_alias_list(aliases);
-	return (*exe_ret);
+int ret = 0, retn;
+int *exe_ret = &retn;
+char *prompt = "$ ", *new_line = "\n";
+name = argv[0];
+hist = 1;
+aliases = NULL;
+signal(SIGINT, _prompt);
+*exe_ret = 0;
+environ = _copyenv();
+if (!environ)
+exit(-100);
+if (argc != 1)
+{
+ret = proc_file_commands(argv[1], exe_ret);
+free_env();
+free_alias_list(aliases);
+return (*exe_ret);
+}
+if (!isatty(STDIN_FILENO))
+{
+while (ret != END_OF_FILE && ret != EXIT)
+ret = handle_args(exe_ret);
+free_env();
+free_alias_list(aliases);
+return (*exe_ret);
+}
+while (1)
+{
+write(STDOUT_FILENO, prompt, 2);
+ret = handle_args(exe_ret);
+if (ret == END_OF_FILE || ret == EXIT)
+{
+if (ret == END_OF_FILE)
+write(STDOUT_FILENO, new_line, 1);
+free_env();
+free_alias_list(aliases);
+exit(*exe_ret);
+}
+}
+free_env();
+free_alias_list(aliases);
+return (*exe_ret);
 }
